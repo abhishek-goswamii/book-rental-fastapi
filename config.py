0752caaeed3cf0f214a074de5050765491fc5be5
abhishek-goswamii <abi.xa12@gmail.com>
2023-05-27 12:45:00 +0530

    rechecking

--- config.py
+++ config.py
@@ -1,22 +1,24 @@
 from pydantic import BaseSettings
 
-class Settings(BaseSettings):
-    DATABASE_USERNAME:str
-    DATABASE_PASSWORD:str
-    SECRET_KEY:str
-    ALGORITHM:str
-    ACCESS_TOKEN_EXPIRE_MINUTES:int
-    SMTPPASSWORD:str
-    HOSTNAME:str
-    PORT:int
-    DATABASE_NAME:str
-    TESTING_DB_NAME:str
-    TESTING_DB_HOST:str
-    TESTING_DB_PORT:int
-    SMTPEMAIL:str
-    SMTPPASSWORD:str
-       
-    class Config:
-        env_file=".env"
 
-settings=Settings()
+class Settings(BaseSettings):
+    DATABASE_USERNAME: str
+    DATABASE_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SMTPPASSWORD: str
+    HOSTNAME: str
+    PORT: int
+    DATABASE_NAME: str
+    TESTING_DB_NAME: str
+    TESTING_DB_HOST: str
+    TESTING_DB_PORT: int
+    SMTPEMAIL: str
+    SMTPPASSWORD: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
